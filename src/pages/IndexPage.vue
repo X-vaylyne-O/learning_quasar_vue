97541a0db0314a0ad1ac49cda1d6ad737395f6a7
Kyle Henderson <kyle.henderson@freightvana.io>
2024-10-13 20:30:25 -0700

    just someindex page stuff

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,19 @@
 <template>
 	<q-page class="flex flex-center">
-		<h1>Lets learn us some stuff!</h1>
-		<img
-			alt="Quasar logo"
-			src="~assets/quasar-logo-vertical.svg"
-			style="width: 200px; height: 200px"
-		/>
+        <div class="row text-center">
+            <div class="col-12">
+                <h5>Lets learn us some stuff!</h5>
+            </div>
+            <div class="col-12">
+                <img
+                    alt="Quasar logo"
+                    src="~assets/quasar-logo-vertical.svg"
+                    style="width: 200px; height: 200px"
+                />
+            </div>
+        </div>
+        <div>
+        </div>
 	</q-page>
 </template>
 
