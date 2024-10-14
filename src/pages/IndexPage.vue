efdfc6849e95723cfc9625a8bc778c14c31ce6f6
Kyle Henderson <kyle.henderson@freightvana.io>
2024-10-13 19:54:30 -0700

    add h1 to index

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,16 @@
 <template>
   <q-page class="flex flex-center">
+    <h1>Lets learn us some stuff!</h1>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-vertical.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
